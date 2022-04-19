24df9e12729160f270a91bcf043640295473b6a5
Huy Vo <vohuy819@gmail.com>
2022-04-19 13:19:25 +0930

    git ingnore

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
-const Student = require('./Student');
-const Course = require('./Course');
+const User = require('./User');
+const Thought = require('./Thought');
+const Reaction = require('./Reaction')
 
-module.exports = { Student, Course };
+module.exports = { User,Thought,Reaction};
